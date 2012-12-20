68744b1609dbf3fcb6a02378def2d09832b02bb2
Stefan Oestreicher <oestef@gmail.com>
2012-12-20 19:03:14 +0100

    harder, faster, clojure

--- src/euler/p4.clj
+++ src/euler/p4.clj
@@ -1,22 +1,21 @@
 (ns euler.p4)
 
-(defn palindrome? [xs]
-  (let [x (seq (str xs))]
-    (if (= 1 (count x))
-      false
-      (= (reverse x) x))))
+(defn palindrome? [n]
+  (if (< n 10)
+    false
+    (let [s (Integer/toString n)]
+      (= s (clojure.string/reverse s)))))
 
 (defn products [max]
-  (distinct
-   (for [x (range 1 max)
-         y (range 1 max)]
-     (* x y))))
+  (let [nums (range (dec max) 0 -1)]
+    (for [x nums y nums]
+      (* x y))))
 
 (defn palindromes [coll]
   (filter palindrome? coll))
 
 (defn solve [n]
-  (apply max (palindromes (products n))))
+  (reduce max (palindromes (products n))))
 
 (def test-with [100 9009])
 (def solve-for 1000)

dc6ae0ebbefe6ac6ecf8b7f71b1c5b58265ea983
Stefan Oestreicher <oestef@gmail.com>
2012-12-23 14:20:06 +0100

    improve performance by about 800%
    This assumes that a and b in a pythagorean triplet where
    a+b+c=n cannot be larger than n/2.
    I have not proved this correct but it seems to work :-)

--- src/euler/p9.clj
+++ src/euler/p9.clj
@@ -5,12 +5,12 @@
                    (square b))))
 
 (defn solve [n]
-  (let [nums (range 1 n)]
+  (let [nums (range 1 (/ n 2))]
     (reduce *
             (first
              (for [a nums b nums c nums
-                   :when (and (< a b c)
-                              (= (+ a b c) n)
+                   :when (and (= (+ a b c) n)
+                              (< a b c)
                               (pythagorean-triplet? a b c))]
                [a b c])))))
 
